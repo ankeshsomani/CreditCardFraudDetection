059d434379127c46b72f949d000702e8cbfa8a7a
Abhishek Murkute <Abhishe13658@Mastek.Com>
2017-06-01 13:47:23 +0530

    alert module modified email module by AS

--- Code/alertmodule/src/main/java/com/masteklabs/app/AlertProvider.java
+++ Code/alertmodule/src/main/java/com/masteklabs/app/AlertProvider.java
@@ -2,59 +2,79 @@ package com.masteklabs.app;
 
 import java.util.Arrays;
 import java.util.Properties;
+import java.util.regex.Pattern;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.log4j.Logger;
 
 public class AlertProvider {
-	
-	
-	public static void main(String[] args) {
-		Properties props = new Properties();
-	     props.put("bootstrap.servers", "localhost:9092");
-	     props.put("group.id", "test");
-	     props.put("enable.auto.commit", "true");
-	     props.put("auto.commit.interval.ms", "1000");
-	     props.put("session.timeout.ms", "30000");
-	     props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-	     props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-	     KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
-	     consumer.subscribe(Arrays.asList("testing3"));
-	     while (true) {
-	         ConsumerRecords<String, String> records = consumer.poll(100);
-	         for (ConsumerRecord<String, String> record : records)
-	             processRecord(record);
-	     }
-		
-	}
-	
-	private static void processRecord(ConsumerRecord<String, String> record){
-		
-		String message=createMessage(record);
-		String subject="ALERT:Suspicious Transaction";
-		String recipientMailAddr=retrieveRecipientAddress();
-		
-		MailHelper.sendMail(recipientMailAddr, subject, message);
-		
-		
-		
-	}
+        static final Logger log = Logger.getLogger(AlertProvider.class.getName());
+        public static final Pattern COMMA_REGEX = Pattern.compile(",");
+        public static final String PREDICTED_EVENTS_TOPIC="testing3";
+        public static final int POLLING_DURATION=100;
+        public static void main(String[] args) {
+                Properties props = new Properties();
+             props.put("bootstrap.servers", "localhost:9092");
+             props.put("group.id", "test");
+             props.put("enable.auto.commit", "true");
+             props.put("auto.commit.interval.ms", "1000");
+             props.put("session.timeout.ms", "30000");
+             props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+             props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+             KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+             try{
+                     consumer.subscribe(Arrays.asList(PREDICTED_EVENTS_TOPIC));
+                     while (true) {
+                         ConsumerRecords<String, String> records = consumer.poll(POLLING_DURATION);
+                         for (ConsumerRecord<String, String> record : records)
+                             processRecord(record);
+                     }
+             }
+             finally{
+                 consumer.close();
+             }
 
-	private static String retrieveRecipientAddress() {
-		// TODO Auto-generated method stub
-		return "tomailid";
-	}
+        }
 
-	private static String createMessage(ConsumerRecord<String, String> record) {
-		String key=record.key();
-		if(key == null){
-			System.out.println("assigning value");
-			key=record.value();
-		}
-		StringBuilder out=new StringBuilder();
-		out.append("Hello, We have received a suspicious transaction from your account.The transaction reference number is "+key+".You will require OTP 123456 to approve the transaction");
-		return out.toString();
-	}
+        private static void processRecord(ConsumerRecord<String, String> record){
+
+                String message=createMessage(record);
+                String subject="ALERT:Suspicious Transaction";
+                String recipientMailAddr=retrieveRecipientAddress(record.value());
+                String prediction=COMMA_REGEX.split(record.value())[1];
+                System.out.println("printing ");
+
+                System.out.println(prediction);
+
+//              if(prediction.equals("0.0")){
+//                      recipientMailAddr="ankesh.somani@mastek.com";
+//              }
+//              else{
+//                      recipientMailAddr="abhishek.murkute@mastek.com";
+//              }
+//              System.out.println("sending mail to "+recipientMailAddr);
+ if(prediction.equals("1.0")){
+
+                MailHelper.sendMail(retrieveRecipientAddress(record.value()), subject, message);
+}
+        }
+
+        private static String retrieveRecipientAddress(String message) {
+                return COMMA_REGEX.split(message)[2];
+        }
+
+        private static String createMessage(ConsumerRecord<String, String> record) {
+                String key=record.key();
+                if(key == null){
+                        System.out.println("assigning value");
+                        key=record.value();
+                }
+                StringBuilder out=new StringBuilder();
+                String transRefNumber=COMMA_REGEX.split(key)[0];
+                out.append("Hello, We have received a suspicious transaction from your account.The transaction reference number is "+transRefNumber+".You will require OTP 123456 to approve the transaction");
+                return out.toString();
+        }
 
 }
--- Code/alertmodule/src/main/java/com/masteklabs/app/MailHelper.java
+++ Code/alertmodule/src/main/java/com/masteklabs/app/MailHelper.java
@@ -10,52 +10,51 @@ import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+import org.apache.log4j.Logger;
+
 public class MailHelper {
-	static Properties props;
-	static {
-		System.out.println("in this block 1");
-		props = new Properties();
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.socketFactory.port", "465");
-		props.put("mail.smtp.socketFactory.class",
-				"javax.net.ssl.SSLSocketFactory");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.port", "465");
-	}
+        static final Logger log = Logger.getLogger(MailHelper.class.getName());
+        static Properties props;
+        static {
+                System.out.println("in this block 1");
+                props = new Properties();
+                props.put("mail.smtp.host", "smtp.gmail.com");
+                props.put("mail.smtp.socketFactory.port", "465");
+                props.put("mail.smtp.socketFactory.class",
+                                "javax.net.ssl.SSLSocketFactory");
+                props.put("mail.smtp.auth", "true");
+                props.put("mail.smtp.port", "465");
+        }
+        public static final String FROM_MAIL_ADDR="somanireyansh@gmail.com";
+        public static final String FROM_MAIL_ADDR_PWD="ankesh007#";
 
-	public static void sendMail(String recipientMailAddr, String subject, String msgBody) {
-		System.out.println("in this block 2");
-		Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication("somanireyansh@gmail.com", "ankesh007#");// change
-																							// accordingly
-			}
-		});
+        public static void sendMail(String recipientMailAddr, String subject, String msgBody) {
+                log.warn("in this block 2");
+                Session session = Session.getDefaultInstance(props, new javax.mail.Authenticator() {
+                        protected PasswordAuthentication getPasswordAuthentication() {
+                                return new PasswordAuthentication(FROM_MAIL_ADDR, FROM_MAIL_ADDR_PWD);// change
+                                                                                                                                                                                        // accordingly
+                        }
+                });
 
-		// compose message
-		try {
-			MimeMessage message = new MimeMessage(session);
-			message.setFrom(new InternetAddress("somanireyansh@gmail.com"));// change
-																			// accordingly
-			message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipientMailAddr));
-			message.setSubject(subject);
-			message.setText(msgBody);
+                // compose message
+                try {
+                        MimeMessage message = new MimeMessage(session);
+                        message.setFrom(new InternetAddress(FROM_MAIL_ADDR));// change
+                                                                                                                                                        // accordingly
+                        message.addRecipient(Message.RecipientType.TO, new InternetAddress(recipientMailAddr));
+                        message.setSubject(subject);
+                        message.setText(msgBody);
 
-			// send message
-			Transport.send(message);
+                        // send message
+                        Transport.send(message);
 
-			System.out.println("message sent successfully");
+                        log.warn("message sent successfully");
 
-		} catch (MessagingException e) {
-			throw new RuntimeException(e);
-		}
+                } catch (MessagingException e) {
+                        throw new RuntimeException(e);
+                }
 
-	}
+        }
 
-	public static void main(String[] args) {
-		String recipientMailAddr = "tomailid";// change
-																// accordingly
-		sendMail(recipientMailAddr, "elloi", "This is a test message3");
-
-	}
 }
